d52f2a0db26bad5cb75b6f16c599115dca2de376
soulman-is-good <soulman.is.good@gmail.com>
2018-06-11 22:30:10 +0200

    Removed leftovers

--- lib/interfaces/storage.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-abstract class StorageInterface {
-  final String name;
-  StorageInterface(this.name);
-  Future<List<int>> read(int start, [int length = 1]);
-  List<int> readSync(int start, [int length = 1]);
-  int size();
-}
