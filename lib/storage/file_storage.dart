b466c45be3060b8fa5c6ee908da5e1944045eb0e
soulman-is-good <soulman.is.good@gmail.com>
2018-06-21 08:25:32 +0200

    Do not close file while read

--- lib/storage/file_storage.dart
+++ lib/storage/file_storage.dart
@@ -3,10 +3,37 @@ import 'dart:async';
 import '../main.dart';
 import './storage.dart';
 
+typedef void VoidCallback();
+
+class TemporaryAccess<T> {
+  T _value;
+  Timer _timer;
+  final VoidCallback _callback;
+
+  TemporaryAccess(Duration duration, this._value, this._callback) {
+    _timer = new Timer(duration, _reset);
+  }
+
+  void _reset() {
+    _value = null;
+    _callback();
+  }
+
+  void setTime(Duration time) {
+    _timer.cancel();
+    _timer = new Timer(time, _reset);
+  }
+
+  T get value => _value; 
+}
+
 class FileStorage extends Storage {
   static final Map<String, File> _files = new Map();
+  final Map<File, TemporaryAccess<RandomAccessFile>> _handlers;
 
-  FileStorage(String name): super(name) {
+  FileStorage(String name):
+  _handlers = new Map(),
+  super(name) {
     if (!_files.containsKey(name)) {
       final String path = Config.get('dbPath') ?? new String.fromEnvironment('DB_PATH');
 
@@ -14,9 +41,55 @@ class FileStorage extends Storage {
     }
   }
 
+  Future<RandomAccessFile> _open(String name) {
+    File file = _files[name];
+    TemporaryAccess access = _handlers[file];
+
+    if (access != null && access.value != null) {
+      access.setTime(new Duration(seconds: 3));
+
+      return new Future.value(access.value);
+    } else {
+      return _files[name].open()
+        .then((RandomAccessFile fileHandler) {
+          _handlers[file] = new TemporaryAccess<RandomAccessFile>(
+            new Duration(seconds: 3),
+            fileHandler,
+            () {
+              fileHandler.closeSync();
+            }
+          );
+
+          return fileHandler;
+        });
+    }
+  }
+
+  RandomAccessFile _openSync(String name) {
+    File file = _files[name];
+    TemporaryAccess access = _handlers[file];
+    if (access != null && access.value != null) {
+      access.setTime(new Duration(seconds: 3));
+
+      return access.value;
+    } else {
+      RandomAccessFile fileHandler = _files[name].openSync();
+
+      _handlers[file] = new TemporaryAccess<RandomAccessFile>(
+        new Duration(seconds: 3),
+        fileHandler,
+        () {
+          fileHandler.closeSync();
+        }
+      );
+
+      return fileHandler;
+    }
+  }
+
   @override
   Future<List<int>> read(int start, [int length = 1]) async {
-    RandomAccessFile file = await _files[name].open();
+    RandomAccessFile file = await _open(name);
 
     await file.setPosition(start);
 
@@ -25,12 +98,10 @@ class FileStorage extends Storage {
 
   @override
   List<int> readSync(int start, [int length = 1]) {
-    RandomAccessFile file = _files[name].openSync();
+    RandomAccessFile file = _openSync(name);
 
     file.setPositionSync(start);
     List<int> buffer = file.readSync(length);
-
-    file.closeSync();
 
     return buffer;
   }
