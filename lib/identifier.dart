d58dd33c8d736896808d0a57e9900e731530a7a2
Soul_man <i@soulman.kz>
2018-07-23 17:03:31 +0000

    Identifier buffer constructor

--- lib/identifier.dart
+++ lib/identifier.dart
@@ -9,22 +9,30 @@ class Identifier {
   static final DateTime _now = new DateTime.now();
   static int _rndOffset = new Random(_now.millisecondsSinceEpoch).nextInt(2147483648);
   final int _increment;
+  final DateTime _created;
+  final int _processId;
 
   Identifier():
+    _processId = pid,
+    _created = _now,
     _increment = (_rndOffset++);
     
+  Identifier.fromByteArray(List<int> bytes):
+    _processId = byteListToInt(bytes.sublist(0, 2)),
+    _created = new DateTime.fromMillisecondsSinceEpoch(byteListToInt(bytes.sublist(2, 6))),
+    _increment = byteListToInt(bytes.sublist(6, 10));
+    
   List<int> toByteArray() => new List<int>()
-    ..addAll(intToByteListBE(pid, 2))
-    ..addAll(intToByteListBE(_now.millisecondsSinceEpoch, 4))
+    ..addAll(intToByteListBE(_processId, 2))
+    ..addAll(intToByteListBE(_created.millisecondsSinceEpoch, 4))
     ..addAll(intToByteListBE(_increment, 4))
     ..toList(growable: false);
 
   String toString() {
-    String pidList = intToByteListBE(pid, 2).map((int byte) => byte.toRadixString(16)).join('');
-    String timeList = intToByteListBE(_now.millisecondsSinceEpoch, 4).map((int byte) => byte.toRadixString(16)).join('');
+    String pidList = intToByteListBE(_processId, 2).map((int byte) => byte.toRadixString(16)).join('');
+    String timeList = intToByteListBE(_created.millisecondsSinceEpoch, 4).map((int byte) => byte.toRadixString(16)).join('');
     String incrementList = intToByteListBE(_increment, 4).map((int byte) => byte.toRadixString(16)).join('');
     
     return '$pidList-$timeList-$incrementList';
-    // return toByteArray().map((int byte) => byte.toRadixString(16)).join('');
   }
 }
